c2d5c338998fb37d949e3fd21864ae50c3ef1a8e
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 20:16:39 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -67,7 +67,7 @@ epic_tragedy[:additional_characters]=[]
 
 
 epic_tragedy[:additional_characters] << {name:"Prince Escalus"}
-epic_tragedy[:additional_characters] << {name: "Apothecary" }
+epic_tragedy[:additional_characters] << {name: "Apothecari" }
    # After your code, the following line will return the altered hash
    epic_tragedy
 end
